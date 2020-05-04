66bd0d19dcb0f3dd68eb2274ecea20d7f4746449
ka-kojima <kojika1018@gmail.com>
2020-05-04 15:53:27 +0900

    フッター

--- k01/assets/js/common.js
+++ k01/assets/js/common.js
@@ -1,38 +1,37 @@
 //共通パーツ読み込み
 $(function() {
-
-　$("#header").load("parts/header.html");
-　$("#footer").load("parts/footer.html");
-
-  // グローバルメニュー
-  $(".btn-gnavi").on("click", function(){
-      // ハンバーガーメニューの位置を設定
-      var rightVal = 0;
-      if($(this).hasClass("open")) {
-          // 位置を移動させメニューを開いた状態にする
-          rightVal = -300;
-          // メニューを開いたら次回クリック時は閉じた状態になるよう設定
-          $(this).removeClass("open");
-      } else {
-          // メニューを開いたら次回クリック時は閉じた状態になるよう設定
-          $(this).addClass("open");
-      }
-      $("#global-navi").stop().animate({
-          right: rightVal
-      }, 200);
-    });
-    $('.menu a[href]').on('click', function(event) {
-    $('.btn-gnavi').trigger('click');
-  });
-
-  // ページトップ
-  var topBtn = $("#page-top-btn");
-    console.log(topBtn);
-  //スルスルっとスクロールでトップへもどる
-  topBtn.click(function () {
-      $('body,html').animate({
-          scrollTop: 0
-      }, 500);
-      return false;
-  });
+  // $("#header").load("parts/header.html");
+  $("#footer").load("parts/footer.html");
 });
+
+// ハンバーガーメニュー
+$(function(){
+    $(".btn-gnavi").on("click", function(){
+        // ハンバーガーメニューの位置を設定
+        var rightVal = 0;
+        if($(this).hasClass("open")) {
+            // 位置を移動させメニューを開いた状態にする
+            rightVal = -300;
+            // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+            $(this).removeClass("open");
+        } else {
+            // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+            $(this).addClass("open");
+        }
+        $("#global-navi").stop().animate({
+            right: rightVal
+        }, 200);
+    });
+});
+
+// ページトップ
+	$(function () {
+	    var topBtn = $('#page-top-btn');
+	    //スルスルっとスクロールでトップへもどる
+	    topBtn.click(function () {
+	        $('body,html').animate({
+	            scrollTop: 0
+	        }, 500);
+	        return false;
+	    });
+	});
