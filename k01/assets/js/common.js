b5a96d3dd7fc22faafbdb33bb1a31e0bc8e8f9e8
ka-kojima <kojika1018@gmail.com>
2020-05-30 00:07:54 +0900

    ナビjs修正

--- k01/assets/js/common.js
+++ k01/assets/js/common.js
@@ -6,18 +6,32 @@ $(window).on("load", function() {
       if($(this).hasClass("open")) {
           // 位置を移動させメニューを開いた状態にする
           rightVal = -300;
-          // メニューを開いたら次回クリック時は閉じた状態になるよう設定
-          $(this).removeClass("open");
-      } else {
-          // メニューを開いたら次回クリック時は閉じた状態になるよう設定
-          $(this).addClass("open");
       }
+      // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+      $(this).toggleClass("open");
       $("#global-navi").stop().animate({
           right: rightVal
       }, 200);
   });
   $(document).on("click", ".menu a[href]", function(event){
       $('.btn-gnavi').trigger('click');
+  });
+
+  // ナビの範囲外のどこかをクリックしたときに発動
+  $(document).on('click', "main", function () {
+      if ($('#global-navi').is(':visible')) {
+          // ナビが表示されていたらclose
+          // 位置を移動させメニューを開いた状態にする
+          var rightVal = -300;
+          // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+          $("#wrapper p").toggleClass("open");
+          $("#global-navi").stop().animate({
+              right: rightVal
+          }, 200);
+      } else {
+          // ナビが非表示の場合は起動しない
+          event.stopPropagation();
+      }
   });
 
   // ページトップ
